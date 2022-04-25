c32762dbc26e38296a753602d3323b778c657fd3
Animila <hristoforovila5@gmail.com>
2022-04-25 17:28:31 +0900

    Add profile btn

--- index.php
+++ index.php
@@ -26,11 +26,14 @@ require_once ("core/databases/dbconfig.php");
         <div class="header__menu">
         <div class="container">
             <img src="icons/Logo.svg" alt="логотип" class="col-md-1 mt-2 header__logo">
-            <button class="offset-md-7 col-md-2 button" onclick="window.location.href='#reg'">
+            <!-- <button class="offset-md-7 col-md-2 button" onclick="window.location.href='#reg'">
                 Регистрация
             </button>
             <button class="col-md-2 button" onclick="window.location.href='#authing'">
                 Вход
+            </button> -->
+            <button class="offset-md-7 col-md-3 button button_profile" onclick="window.location.href='profile.php'">
+                Владимир Владимирович
             </button>
         </div>
     </div>
