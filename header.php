17014e4f50e070dd6eac0a80971ef1ea03f289af
AgniaEndie <Endienasg2@gmail.com>
2022-04-25 15:37:30 +0900

    optimisation 1

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php ?>
+<header class="header">
+    <div class="header__menu">
+        <div class="container">
+            <img src="icons/Logo.svg" alt="логотип" class="col-md-1 mt-2 header__logo">
+            <button class="offset-md-7 col-md-2 button" onclick="window.location.href='#reg'">
+                Регистрация
+            </button>
+            <button class="col-md-2 button" onclick="window.location.href='#authing'">
+                Вход
+            </button>
+        </div>
+    </div>
+</header>
